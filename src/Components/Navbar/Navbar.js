8e9288155b7f45c85d008a77388c0fd850c658cf
jaguso <jagutzo@gmail.com>
2018-10-28 20:17:00 -0600

    added signup and login forms

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './style.scss';
+ class Navbar extends Component {
+   render() {
+    return(
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Sin delantal</a>
+         <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="">Login</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">SignUp</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+ 
+
+export default Navbar;
